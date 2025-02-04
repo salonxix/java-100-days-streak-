fbb06ccb6669edb897cade5c19d45107d9cba10a
Saloni Kumari <saloni.kumari12@s.amity.edu>
2025-02-04 15:20:33 +0530

    Add files via upload
    🚀 Java User Input Program | Day 4 of 100
    In this program, I have implemented a Java console application that takes three integer inputs from the user using the Scanner class and calculates their sum. The program prompts the user to enter three numbers, adds them, and then displays the total.
    
    This simple program helps in understanding the basics of user input handling in Java, working with integers, and performing basic arithmetic operations.
    
    📌 Features & Concepts Covered:
    ✅ User Input Handling using the Scanner class
    ✅ Integer Data Type for numerical operations
    ✅ Basic Arithmetic Operations (Addition)
    ✅ Console Output using System.out.println()
    📌 #100DaysOfCode #Java #UserInput #ScannerClass #JavaProgramming #CodingJourney #CodeNewbie #GitHub #LearnJava #ProgrammingLife

---
+++ userinput.java
@@ -0,0 +1,15 @@
+ import java.util.*;
+class userinput{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+System.out.print("enter first number:");
+int a=sc.nextInt();
+System.out.print("enter second number:");
+int b=sc.nextInt();
+System.out.print("enter third number:");
+int c=sc.nextInt();
+int d=a+b+c;
+System.out.println("total="+d);
+}
+}
+ 
